ebebb7a1aa5dd35c64e9be1374f9d09787be7d06
Daniel Sun <danieljsun@gmail.com>
2013-07-07 16:28:49 -0400

    first 100 primes

--- startup/hw1/part2.js
+++ startup/hw1/part2.js
@@ -1,12 +1,33 @@
 #! /usr/bin/env node
 /* global require, console, __filename */
-var fs = require('fs');
-var outfile = "primes.txt";
-var primes = [2,3,5,7,11];
-var out = primes.join(',');
-fs.writeFileSync(outfile, out);
+(function() {
+	'use strict';
+	var fs = require('fs');
+	var outfile = "primes.txt";
+	var primes = [];
+	var n = 2;
+	while (primes.length < 100) {
+		if (isPrime(n)) {
+			primes.push(n);
+		}
+		n++;
+	}
+	var out = primes.join(',');
+	fs.writeFileSync(outfile, out);
 
-function isPrime(x) {
-	return true;
-}
-console.log('Script: ' + __filename + '\nWrote: ' + out + '\nTo: ' + outfile);
+	function isPrime(x) {
+		var prime = true;
+		var lower = 2;
+		var upper = Math.sqrt(x);
+		var d = lower;
+		while (d <= upper) {
+			if (x % d === 0) {
+				prime = false;
+				break;
+			}
+			d++;
+		}
+		return prime;
+	}
+	console.log('Script: ' + __filename + '\nWrote: ' + out + '\nTo: ' + outfile);
+})();
