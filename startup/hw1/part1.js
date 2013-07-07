dddaf62cb805495d10ffa5442797168abb0d5320
Daniel Sun <danieljsun@gmail.com>
2013-07-07 16:09:53 -0400

    Revert "added missing newline"
    This reverts commit a3b423c04e0b8d82ac79d91ad755a65f4dccc8dc.

--- startup/hw1/part1.js
+++ startup/hw1/part1.js
@@ -1,9 +1,7 @@
 #! /usr/bin/env node
 /* global require, console, __filename */
 var fs = require('fs');
-var outfile = "primes.txt";
-var primes = [2,3,5,7,11];
-var out = primes.join(',');
+var outfile = "hello.txt";
+var out = "A startup is a business built to grow rapidly.";
 fs.writeFileSync(outfile, out);
-
-console.log('Script: ' + __filename + '\nWrote: ' + out + 'To: ' + outfile);
+console.log('Script: ' + __filename + '\nWrote: ' + out + '\nTo: ' + outfile);
